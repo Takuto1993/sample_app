0613d6711aa73a9d2b02ccabbba666a8dff7eacf
EC2 Default User <ec2-user@ip-172-31-12-185.ec2.internal>
2022-03-10 07:29:38 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,14 @@ class ListsController < ApplicationController
 
   def create
     list = List.new(list_params)
-    
+
     list.save
-    
+
     redirect_to '/top'
   end
 
   def index
+    #@lists = List.all
   end
 
   def show
@@ -19,7 +20,7 @@ class ListsController < ApplicationController
 
   def edit
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
-  get 'lists/index'
+  get 'top' => 'homes#top'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
   get '/top' => "homes#top"
